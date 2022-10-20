919916bb84474147d8e8ea39de078e722196a97e
daroshIrani <darosh29@gmail.com>
2022-10-20 06:07:10 -0400

    Changes  Reverted

--- config/config.js
+++ config/config.js
@@ -6,12 +6,12 @@
 // session information is to be set here and global variables
 export const Secret = "MySecret"
 
-// export const MongoURI = "mongodb://127.0.0.1/media" // const name can be any, and you connect the code from 'mongosh' here as the address of your datatbase during production 
+export const MongoURI = "mongodb://127.0.0.1/media" // const name can be any, and you connect the code from 'mongosh' here as the address of your datatbase during production 
                                             // stage because db is local rightnow  - you do not need the actual prt number because mongo always connects on port 27017 anyway - Remmeber
                                             // to run the 'mongosh' command in a seperate cmd prompt after running 'mongod' and starting the server
                                             // Rememebr - /media is added so that we tell our that we are starting a new database called 'media' at the provided address above
                                             //              and 127.0.0.1 is local host anyway
 
-export const MongoURI = "mongodb+srv://darosh:Wvam6305161!@comp229-week5.hllnhbm.mongodb.net/media1?retryWrites=true&w=majority" // we use media1 same way we used the connectipon to media up above - at this point
+//export const MongoURI = "mongodb+srv://darosh:Wvam6305161!@comp229-week5.hllnhbm.mongodb.net/media1?retryWrites=true&w=majority" // we use media1 same way we used the connectipon to media up above - at this point
                                                                                                                                  // we do not need the local mongod or mongosh connection 
                                                                                                                                  // make sure you add the ip 0.0.0.0/0 to allow anyone to access it with permiosisons
